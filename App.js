d8e287698d5c2bb44ae07071ddd53b2f029ccf7f
Test <jiteshrp@gmail.com>
2019-02-27 22:12:59 +0530

    changes in receiving data from worker thread

--- App.js
+++ App.js
@@ -5,10 +5,11 @@ var countOddNumbers = () => {
   randomWorker.postMessage(numInputValue);
 }
 
-randomWorker.onmessage((e) => {
-  document.getElementById('resultNum').innerText = e;
-})
-
 var returnRandomNumber = () => {
     document.getElementById('randomNum').innerText = Math.random();
+}
+
+randomWorker.onmessage = e => {
+  console.log('message received from worker thread');
+  document.getElementById('resultNum').innerText = e.data;
 }
